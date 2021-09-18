5ea15185d4bf6b1689bd7abe01133494f4ab81b3
aslanovasadbekjon <87481245+aslanovasadbekjon@users.noreply.github.com>
2021-09-18 09:23:18 +0500

    rasm ham ishlayapti

--- dedline_news/news/views.py
+++ dedline_news/news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import News
+from .models import News,Category
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
         'title': "Yangilklar ro`yxati",
+        'categories': categories,
 
     }
     return render(request,template_name='news/index.html',context=context)
