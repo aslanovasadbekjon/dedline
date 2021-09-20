b1736147e764bb6ed9668b04c9b9571c529028e1
aslanovasadbekjon <87481245+aslanovasadbekjon@users.noreply.github.com>
2021-09-20 07:07:29 +0500

    12-dars ham tayyor
    form 2- qism ham tayyor

--- dedline_news/news/forms.py
+++ dedline_news/news/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150,label="Sarlavha")
-    content = forms.CharField(label="Yangilik matni",required=False)
+    title = forms.CharField(max_length=150,label="Sarlavha", widget=forms.TextInput(attrs={"class":"form-control"}))
+    content = forms.CharField(label="Yangilik matni",required=False,widget=forms.Textarea(attrs={"class":"form-control","rows":5}))
     is_published = forms.BooleanField(label="Chop qilinganligi",initial=True)
-    category = forms.ModelChoiceField(label="Kategoriya",empty_label='Kategoriyani tanlang',queryset=Category.objects.all())
+    category = forms.ModelChoiceField(label="Kategoriya",empty_label='Kategoriyani tanlang',queryset=Category.objects.all(),widget=forms.Select(attrs={"class":"form-control"}))
