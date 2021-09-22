a3bd3ae65ebf78c5774e1f5933922c56286f063e
aslanovasadbekjon <87481245+aslanovasadbekjon@users.noreply.github.com>
2021-09-22 09:50:21 +0500

    14-dars ham tayyor
    List view sinfi tugadi

--- dedline_news/news/views.py
+++ dedline_news/news/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import News, Category
 from .forms import NewsForm
+from django.views.generic import ListView
+#def index(request):
+    # news = News.objects.all()
+    # #categories = Category.objects.all()
+    # context = {
+    #     'news': news,
+    #     'title': "Yangilklar ro`yxati",
+    #     #'categories': categories,
+    #
+    # }
+    # return render(request,template_name='news/index.html',context=context)
 
-def index(request):
-    news = News.objects.all()
-    #categories = Category.objects.all()
-    context = {
-        'news': news,
-        'title': "Yangilklar ro`yxati",
-        #'categories': categories,
-
-    }
-    return render(request,template_name='news/index.html',context=context)
-
-def get_category(request,category_id):
-    news = News.objects.filter(category_id = category_id)
-   # categories = Category.objects.all()
-    category = Category.objects.get(pk=category_id)
-    context = {
-        'news': news,
-        #'categories': categories,
-        'category': category
-    }
-    return render(request, template_name='news/category.html', context = context)
+# def get_category(request,category_id):
+#     news = News.objects.filter(category_id = category_id)
+#    # categories = Category.objects.all()
+#     category = Category.objects.get(pk=category_id)
+#     context = {
+#         'news': news,
+#         #'categories': categories,
+#         'category': category
+#     }
+#     return render(request, template_name='news/category.html', context = context)
 
 def view_news(request,news_id):
     #news_item = News.objects.get(pk=news_id)
@@ -43,3 +43,32 @@ def add_news(request):
 
     return render(request, 'news/add_news.html', {'form':form})
 
+#######
+
+class BoshNews(ListView):
+    model = News
+    template_name = 'news/home_page.html'
+    context_object_name = 'news'
+    #extra_context = {'title':'Bosh sahifa'}
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Bosh sahifa'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_page.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'],is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
