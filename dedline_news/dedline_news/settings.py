7749cf4505b135175cb6f2176ff76606a519b0ba
aslanovasadbekjon <87481245+aslanovasadbekjon@users.noreply.github.com>
2021-09-26 11:01:08 +0500

    postgresqlga ulandi

--- dedline_news/dedline_news/settings.py
+++ dedline_news/dedline_news/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'dedline_news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'News',
+        'USER': 'backend',
+        'PASSWORD': '2409',
     }
 }
 
